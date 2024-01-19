d57ebd4bd0309dfa7908fc6d07188c3bdb50b879
KaishAli <360mohdkaishali.cs@gmail.com>
2024-01-20 00:53:42 +0530

    Login Component implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Login from '../src/components/Login/Login'
 import ExperienceEngage from '../src/components/Experience Engage/ExperienceEngage'
 import MeetMasters from '../src/components/Masters/MeetMasters'
 import Header from '../src/components/Header/Header'
@@ -6,27 +7,36 @@ import Footer from '../src/components/Footer/Footer'
 import NewsTab from '../src/components/News Section/NewsTab'
 import Community from './components/Community/Community'
 import Apply from '../src/components/Apply/Apply'
-
+import { useState } from 'react'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <>
       <div className='main-wp'>
-        <div>
-        <Header />
-        </div>
-        <hr style={{ color: "white" }} />
-        <Hero />
-        <Apply />
-        <ExperienceEngage />
-        <MeetMasters />
-        <NewsTab />
-        <Community />
-        <Footer />
+        {
+          isAuthenticated ? <>
+            <div>
+              <Header />
+            </div>
+            <hr style={{ color: "white" }} />
+            <Hero />
+            <Apply />
+            <ExperienceEngage />
+            <MeetMasters />
+            <NewsTab />
+            <Community />
+            <Footer />
+          </>
+            : <Login setIsAuthenticated={setIsAuthenticated} />
+        }
       </div>
+
 
     </>
   );
+
 }
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import '../../components/Login/Login.css'
+function Login({ setIsAuthenticated }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [Authen, setAuthen] = useState(false);
+
+    useEffect(() => {
+    }, [Authen]);
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (password && email) {
+            setAuthen(true);
+            setIsAuthenticated(true);
+        } else {
+            setAuthen(false);
+            setIsAuthenticated(false);
+        }
+        console.log(Authen); // This will still log the previous state value
+    }
+    return (
+        <>
+        
+            <div className="Logcontainer">
+                <img
+                    src="https://www.yourwebsite.com/path/to/your/logo.png"
+                    alt="Your Logo"
+                />
+                <h1 className="textColor">Sign In</h1>
+                <form id="signinForm" onSubmit={handleSubmit}>
+                    <label className="textColor" htmlFor="email">
+                        Email or phone number:
+                    </label>
+                    <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="text" id="email" name="email" required="" />
+                    <br />
+                    <label className="textColor" htmlFor="password">
+                        Password:
+                    </label>
+                    <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" id="password" name="password" required="" />
+                    <br />
+                    <br />
+                    <button className='sbm' type="submit">Sign In</button>
+                </form>
+            </div>
+
+        </>
+    )
+}
+export default Login;
